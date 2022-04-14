ec8f650b7ab4d33eeafb4998505498a841bb4a11
kanyadevs <sukanya.chatterjee@airwalkconsulting.com>
2022-04-14 12:37:13 +0100

    Lab-17 local-exec

--- Lab-17/main.tf
+++ Lab-17/main.tf
@@ -17,3 +17,50 @@ resource "null_resource" "command1" {
     }
 }
 
+resource "null_resource" "command2" {
+    provisioner "local-exec" {
+        command = "ping -c 5 www.google.com"
+    }
+}
+
+resource "null_resource" "command3" {
+    provisioner "local-exec" {
+        interpreter = ["python", "-c"]
+        command = "print('Hello World from Python!)"
+    }
+}
+
+resource "null_resource" "command4" {
+    provisioner "local-exec" {
+        command = "echo $NAME1 $NAME2 $NAME3 >> names.txt"
+        environment = {
+          NAME1 = "Bob"
+          NAME2 = "Mark"
+          NAME3 = "John"
+         }
+    }
+}
+
+resource "aws_instance" "myserver" {
+    ami = "ami-03e88be9ecff64781"
+    instance_type = "t2.nano"
+    provisioner "local-exec" {
+        command = "echo ${aws_instance.myserver.private_ip} >> log.txt"
+    }
+}
+
+resource "null_resource" "command5" {
+    provisioner "local-exec" {
+        command = "echo Terraform FINISH: $(date) >> log.txt"
+    }
+
+    depends_on = [
+      null_resource.command1,
+      null_resource.command2,
+      null_resource.command3,
+      null_resource.command4,
+      null_resource.command5,
+      aws_instance.myserver
+    ]
+  
+}
